b476e11d9a6ee7a3b2a06ee490fcd5520703a37b
Nicola Casagrande <dart1997@live.it>
2023-05-18 12:08:35 +0200

    Add project files.

---
+++ CombatTracker/CombatTracker.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace CombatTracker
+{
+    partial class CombatTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.col_Iniziativa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_PF = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_Iniziativa,
+            this.col_Nome,
+            this.col_PF,
+            this.col_Status});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(667, 333);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            // 
+            // col_Iniziativa
+            // 
+            this.col_Iniziativa.HeaderText = "Iniziativa";
+            this.col_Iniziativa.Name = "col_Iniziativa";
+            this.col_Iniziativa.ReadOnly = true;
+            // 
+            // col_Nome
+            // 
+            this.col_Nome.HeaderText = "Nome";
+            this.col_Nome.Name = "col_Nome";
+            this.col_Nome.ReadOnly = true;
+            // 
+            // col_PF
+            // 
+            this.col_PF.HeaderText = "PF";
+            this.col_PF.Name = "col_PF";
+            this.col_PF.ReadOnly = true;
+            // 
+            // col_Status
+            // 
+            this.col_Status.HeaderText = "Status";
+            this.col_Status.Name = "col_Status";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Add.Location = new System.Drawing.Point(499, 269);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Reset.Location = new System.Drawing.Point(580, 298);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_Reset.TabIndex = 3;
+            this.btn_Reset.Text = "RESET";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Remove.Location = new System.Drawing.Point(580, 269);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(75, 23);
+            this.btn_Remove.TabIndex = 4;
+            this.btn_Remove.Text = "Remove";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // CombatTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 333);
+            this.Controls.Add(this.btn_Remove);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CombatTracker";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Iniziativa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_PF;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Status;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Remove;
+    }
+}
+
---
+++ CombatTracker/CombatTracker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombatTracker
+{
+    public partial class CombatTracker : Form
+    {
+        public CombatTracker()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            var ris = MessageBox.Show("Sei sicuro?", "Attenzione!", MessageBoxButtons.YesNo);
+            if (ris == DialogResult.Yes)
+                dataGridView1.Rows.Clear();
+        }
+
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(index);
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+
+            //MessageBox.Show("AGGIUNTA");
+        }
+    }
+}
